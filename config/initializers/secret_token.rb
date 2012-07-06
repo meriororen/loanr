5c4f5ba80350a81d9addcba0ed578a0f18c62ee3
meriororen <meriororen@gmail.com>
2012-07-06 17:12:12 +0900

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Loanr::Application.config.secret_token = '0d6c801cfa8be86c71647bf8854d70d7d23c68a41bfa08507137ecf739c22a6744154255e89777d8399d0c6b6a639431caea7b6d79d9eef18a1c5b12cfeac889'
